a50a3b572b8ed9f8f9d892a5b0ddd2f60664eab1
Mark Stabler <mark@stablerwritingservices.com>
2020-03-02 21:29:12 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,5 @@
+array = [1, 2, 3, 4]
+
 def square_array(array)
   squared_num = []
   array.each do |num|
@@ -5,3 +7,13 @@ def square_array(array)
   end
   squared_num
 end
+
+
+def square_array_collect(array)
+  array.collect do |num|
+    num**2
+  end
+end
+
+puts square_array(array)
+puts square_array_collect(array)
