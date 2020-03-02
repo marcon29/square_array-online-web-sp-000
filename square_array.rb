7a0df836ca22c202bc4c1f4091ebb489c348efe8
Mark Stabler <mark@stablerwritingservices.com>
2020-03-02 21:28:11 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -7,3 +7,13 @@ def square_array(array)
   end
   squared_num
 end
+
+
+def square_array_collect(array)
+  array.collect do |num|
+    num**2
+  end
+end
+
+puts square_array(array)
+puts square_array_collect(array)
